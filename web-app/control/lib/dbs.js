f9cb55129848c5d9fc2dc77b03396c6dbe3d3bcd
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-04-05 20:22:58 +0800

    setting up centralized query

--- web-app/control/lib/dbs.js
+++ web-app/control/lib/dbs.js
@@ -11,6 +11,31 @@ const createDB = (port) => mysql.createConnection({
     multipleStatements: true
 });
 
+function query(db) {
+    return async (q, values, mode) => {
+        try {
+            await db.query('LOCK TABLES movies ' + mode)
+            const results = await db.query(q, values)
+            await db.query('UNLOCK TABLES')
+            await db.end()
+            return results
+        } catch (e) {
+            try {
+            await db.query('ROLLBACK')
+            await db.query('UNLOCK TABLES')
+            await db.end()
+            } catch (e) {
+            throw Error(e.message)
+            }
+            throw Error(e.message)
+        }
+    }
+}
+
 export const db1 = createDB(process.env.PORT0);
 export const db2 = createDB(process.env.PORT1);
 export const db3 = createDB(process.env.PORT2);
+
+export const query1 = async (q, values, mode) => await query(db1)(q, values, mode)
+export const query2 = async (q, values, mode) => await query(db2)(q, values, mode)
+export const query3 = async (q, values, mode) => await query(db3)(q, values, mode)
