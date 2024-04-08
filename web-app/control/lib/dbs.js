70429e3c25ce28baf822132a906ff5f5e5effa3a
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-04-08 13:57:28 +0800

    fix: db and transition

--- web-app/control/lib/dbs.js
+++ web-app/control/lib/dbs.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
 
-function query(db, table) {
+function query(db) {
     return async (q, values, mode) => {
         try {
             await db.promise().query('LOCK TABLES appointments ' + mode)
@@ -36,6 +36,6 @@ export const db1 = createDB(process.env.PORT0, process.env.DB0);
 export const db2 = createDB(process.env.PORT1, process.env.DB1);
 export const db3 = createDB(process.env.PORT2, process.env.DB2);
 
-export const query1 = async (q, values, mode) => await query(db1, process.env.TABLE0)(q, values, mode);
-export const query2 = async (q, values, mode) => await query(db2, process.env.TABLE1)(q, values, mode);
-export const query3 = async (q, values, mode) => await query(db3, process.env.TABLE2)(q, values, mode);
+export const query1 = async (q, values, mode) => await query(db1)(q, values, mode);
+export const query2 = async (q, values, mode) => await query(db2)(q, values, mode);
+export const query3 = async (q, values, mode) => await query(db3)(q, values, mode);
