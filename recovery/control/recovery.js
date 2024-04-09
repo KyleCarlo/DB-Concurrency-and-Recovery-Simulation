6a64b5aa6334ff62bf9bd0bb9ebd3e9e2d2c407a
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-04-10 06:28:58 +0800

    fix: logging and rep

--- recovery/control/recovery.js
+++ recovery/control/recovery.js
@@ -3,33 +3,56 @@ import {query1, query2, query3} from "./lib/dbs.js";
 const queries = [query1, query2, query3];
 
 async function replicate(source_id, source_node, target_node){
-    let source_latest = (await queries[source_node]('SELECT * FROM logs WHERE id = ? AND node = ?;', [source_id, target_node], 'logs', 'READ'))[0];
-    let action = source_latest.action;
-    
-    delete source_latest.id;
-    delete source_latest.action_time;
-    delete source_latest.action;
-    delete source_latest.node;
-    switch (action) {
-        case 'INSERT':
-            await queries[target_node]('INSERT INTO appointments SET ?;', source_latest, 'appointments', 'WRITE');
-            break;
-        case 'UPDATE':
-            console.log(source_latest, source_latest.apptid);
-            await queries[target_node]('UPDATE appointments SET ? WHERE apptid = ?;', [source_latest, source_latest.apptid], 'appointments', 'WRITE');
-            break;
-        case 'DELETE':
-            await queries[target_node]('DELETE FROM appointments WHERE apptid = ?;', source_latest.apptid, 'appointments', 'WRITE');
-            break;
+    var node;
+    if (source_node == 0) {
+        node = target_node == 1 ? "node_2" : "node_3";
+    } else {
+        node = source_node == 1 ? "node_2" : "node_3";
+    }
+    var data_toRep = await queries[source_node]('SELECT * FROM logs WHERE id >= ? AND node = ?;', [source_id, node], 'logs', 'READ');
+    var repLogs = [];
+    var statements = '';
+    var values = [];
+    for (let index = 0; index < data_toRep.length; index++) {
+        let action = data_toRep[index].action;
+        let id = data_toRep[index].id;
+        delete data_toRep[index].id;
+        delete data_toRep[index].action_time;
+        delete data_toRep[index].action;
+        delete data_toRep[index].node;
+
+        switch (action) {
+            case 'INSERT':
+                statements += 'INSERT INTO appointments SET ?;';
+                values.push(data_toRep[index]);
+                // await queries[target_node]('INSERT INTO appointments SET ?;', source_latest, 'appointments', 'WRITE');
+                break;
+            case 'UPDATE':
+                statements += 'UPDATE appointments SET ? WHERE apptid = ?;';
+                values.push(data_toRep[index]);
+                values.push(data_toRep[index].apptid);
+                // await queries[target_node]('UPDATE appointments SET ? WHERE apptid = ?;', [source_latest, source_latest.apptid], 'appointments', 'WRITE');
+                break;
+            case 'DELETE':
+                statements += 'DELETE FROM appointments WHERE apptid = ?;';
+                values.push(data_toRep[index]);
+                values.push(data_toRep[index].apptid);
+                // await queries[target_node]('DELETE FROM appointments WHERE apptid = ?;', source_latest.apptid, 'appointments', 'WRITE');
+                break;
+        }
+
+        repLogs.push({
+            logId: id, 
+            timestamp: null, 
+            source: source_node,
+            target: target_node,
+            action: action
+        });
     }
 
-    return {
-        logId: source_id,
-        timestamp: new Date(),
-        source: source_node,
-        target: target_node,
-        action: action
-    }
+    await queries[target_node](statements, values, 'appointments', 'WRITE');
+
+    return {timestamp: new Date(), logs: repLogs};
 }
 
 export const recovery = async (config) => {
