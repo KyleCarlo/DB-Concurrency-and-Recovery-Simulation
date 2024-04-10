614513f305eb85c91223b59fe07da35aa8923755
Sealtiel Dy <sealtieldy74@gmail.com>
2024-04-10 18:34:29 +0800

    feat: add concurrent write-write test

---
+++ web-app/__test__/write_write.test.js
@@ -0,0 +1,85 @@
+const puppeteer = require('puppeteer');
+const query = require('../control/lib/dbs.js').query1
+jest.setTimeout(60000);
+
+
+it('read write concurrency test', async () => {
+    const ids = [
+        'A3497E1AD4C71E62AFFAD4947BF12BE0',
+        '8349958C2F977BB2B39ACCA84203E2FA'
+    ]
+    const queryLuzon  = await query("SELECT * FROM appointments WHERE apptid = ?", ids[0], 'READ');
+    const queryVismin = await query("SELECT * FROM appointments WHERE apptid = ?", ids[1], 'READ');
+    const testName = 'write-write test';
+
+    const width = 1920;
+    const height = 1080;
+    const windowSize = '--window-size=' + width + ',' + height;
+    const slowMo = 0; // in ms
+    const browsers = [
+        await puppeteer.launch({
+            headless: false,
+            slowMo: slowMo,
+            args: [windowSize]
+        }),
+        await puppeteer.launch({
+            headless: false,
+            slowMo: slowMo,
+            args: [windowSize]
+        }),
+        await puppeteer.launch({
+            headless: false,
+            slowMo: slowMo,
+            args: [windowSize]
+        }),
+        await puppeteer.launch({
+            headless: false,
+            slowMo: slowMo,
+            args: [windowSize]
+        })
+    ];
+    
+    let pages = [
+        [   // luzon node
+            await browsers[0].pages(),
+            await browsers[1].pages()
+        ],
+        [   // vismin node
+            await browsers[2].pages(),
+            await browsers[3].pages()
+        ]
+    ]
+    pages = [ // idk why browsers[0].pages()[0] does not work to select blank page
+        [
+            pages[0][0][0],
+            pages[0][1][0]
+        ],
+        [
+            pages[1][0][0],
+            pages[1][1][0]
+        ],
+    ]
+    for (var i = 0; i < 2; i++) {
+        for (var j = 0; j < 2; j++) {
+            await pages[i][j].goto('http://localhost:3000/');
+            await pages[i][j].setViewport({width: width, height: height});
+            await pages[i][j].click('#update');
+            await pages[i][j].locator('#update-id').fill(ids[i]);
+            await pages[i][j].locator('#update-name').fill(testName + j);
+        }
+    }
+    setTimeout(function() {
+        pages[0][0].click('#update-button');    // 3, 3, 2
+        pages[0][1].click('#update-button');    // 1, 1, 1
+        pages[1][0].click('#update-button');    // 2, 4, 4
+        pages[1][1].click('#update-button');    // 4, 2, 3
+    },  4000);
+    await pages[1][1].waitForNetworkIdle(200);
+
+    setTimeout(async function() {   // no test because we don't know which write is first
+        await browsers[0].close();
+        await browsers[1].close();
+        await browsers[2].close();
+        await browsers[3].close();
+    }, 4000);
+});
